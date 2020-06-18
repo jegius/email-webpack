d94af7315a09ec5b2e07178dbc580ae541344bea
Ivan Alekseev <alekseev@haulmont.com>
2020-06-18 15:02:22 +0400

    Init

---
+++ jsx/content.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolore inventore pariatur.
+                Cupiditate earum id perferendis unde voluptatum. Accusamus dicta dignissimos distinctio earum fuga
+                minima molestiae nam quam ut, vitae.
+            </div>
+        )
+    }
+}
+
+module.exports = Content
